718e147f57a98c52f5a308c099ef879fed742b9e
David Chaves <dachafra@gmail.com>
2018-01-19 16:47:05 +0100

    [ADD] new stopTimes function

--- src/main/java/dchaves/oeg/dia/fi/upm/es/Freq2StopTimes.java
+++ src/main/java/dchaves/oeg/dia/fi/upm/es/Freq2StopTimes.java
@@ -1,11 +1,11 @@
 package dchaves.oeg.dia.fi.upm.es;
 
 import org.onebusaway.gtfs.impl.GtfsDaoImpl;
-import org.onebusaway.gtfs.model.AgencyAndId;
-import org.onebusaway.gtfs.model.Frequency;
-import org.onebusaway.gtfs.model.StopTime;
-import org.onebusaway.gtfs.model.Trip;
+import org.onebusaway.gtfs.model.*;
+import org.onebusaway.gtfs.serialization.GtfsWriter;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.*;
 import java.util.*;
@@ -13,10 +13,14 @@ import java.util.*;
 public class Freq2StopTimes {
     private GtfsDaoImpl newStore;
     private GtfsDaoImpl oldStore;
+    private HashMap<String, String> newTrips;
+    private Integer globalIdStopTimes;
 
     public Freq2StopTimes(GtfsDaoImpl oldStore){
         this.newStore = new GtfsDaoImpl();
         this.oldStore = oldStore;
+        newTrips = new HashMap<>();
+        globalIdStopTimes=1;
     }
 
     //edit the ids of the trips with the departureTime extracted from frequencies
@@ -24,19 +28,18 @@ public class Freq2StopTimes {
         Collection<Trip> trips=oldStore.getAllTrips();
         Collection<StopTime> stopTimes = oldStore.getAllStopTimes();
         Collection<Frequency> frequencies = oldStore.getAllFrequencies();
-        HashMap<String,Integer> firstStopTime = new HashMap<>();
-        LocalTime midnight = LocalTime.MIDNIGHT;
-        LocalDate today = LocalDate.now(ZoneId.of("Europe/Madrid"));
-        LocalDateTime todayMidnight = LocalDateTime.of(today, midnight);
+        HashMap<String,StopTime> firstStopTime = new HashMap<>();
+        HashMap<String,ArrayList<StopTime>> sumaryStopTimes = this.sumaryStopTimes();
+        LocalDateTime todayMidnight = LocalDateTime.of(LocalDate.now(ZoneId.of("Europe/Madrid")),LocalTime.MIDNIGHT);
 
         //get the first stop of each trip
         stopTimes.forEach(stopTime -> {
             if(!firstStopTime.containsKey(stopTime.getTrip().getId().getId())){
-                firstStopTime.put(stopTime.getTrip().getId().getId(),stopTime.getArrivalTime());
+                firstStopTime.put(stopTime.getTrip().getId().getId(),stopTime);
             }
             else{
-                if(stopTime.getArrivalTime()<firstStopTime.get(stopTime.getTrip().getId().getId())){
-                    firstStopTime.replace(stopTime.getTrip().getId().getId(),stopTime.getArrivalTime());
+                if(stopTime.getArrivalTime()<firstStopTime.get(stopTime.getTrip().getId().getId()).getArrivalTime()){
+                    firstStopTime.put(stopTime.getTrip().getId().getId(),stopTime);
                 }
             }
         });
@@ -45,12 +48,13 @@ public class Freq2StopTimes {
         trips.forEach(trip -> {
             for (String key : firstStopTime.keySet()) {
                 if (trip.getId().getId().equals(key)) {
-                    LocalDateTime timeFirstStop = todayMidnight.plusSeconds(firstStopTime.get(key)).minusHours(1);
+                    LocalDateTime timeFirstStop = todayMidnight.plusSeconds(firstStopTime.get(key).getArrivalTime()).minusHours(1);
                     Trip t = new Trip(trip);
                     t.setId(new AgencyAndId());
                     t.getId().setId(trip.getId().getId() + "_" + new SimpleDateFormat("HH:mm:ss").format(Date.from(timeFirstStop.toInstant(ZoneOffset.UTC))));
                     t.getId().setAgencyId(trip.getId().getAgencyId());
                     newStore.saveEntity(t);
+                    newTrips.put(trip.getId().getId(),new SimpleDateFormat("HH:mm:ss").format(Date.from(timeFirstStop.toInstant(ZoneOffset.UTC))));
                 }
             }
         });
@@ -64,24 +68,66 @@ public class Freq2StopTimes {
                     for(int i=0; i<times;i++){
                         Trip t = new Trip(trip);
                         t.setId(new AgencyAndId());
-                        LocalDateTime timeFirstStop=todayMidnight.plusSeconds(frequency.getStartTime()+frequency.getHeadwaySecs()*i).minusHours(1);
-                        t.getId().setId(trip.getId().getId() + "_" + new SimpleDateFormat("HH:mm:ss").format(Date.from(timeFirstStop.toInstant(ZoneOffset.UTC))));
+                        Integer departureTime = frequency.getStartTime()+frequency.getHeadwaySecs()*i;
+                        LocalDateTime departureDate=todayMidnight.plusSeconds(frequency.getStartTime()+frequency.getHeadwaySecs()*i).minusHours(1);
+                        String newId=trip.getId().getId() + "_" + new SimpleDateFormat("HH:mm:ss").format(Date.from(departureDate.toInstant(ZoneOffset.UTC)));
+                        t.getId().setId(newId);
                         t.getId().setAgencyId(trip.getId().getAgencyId());
                         newStore.saveEntity(t);
+                        createNewStopTimes(departureTime,(ArrayList<StopTime>)sumaryStopTimes.get(trip.getId().getId()).clone(),newId,firstStopTime.get(trip.getId().getId()));
                     }
 
                 }
             });
         });
+        write();
+    }
+
+
+    public void createNewStopTimes(Integer departureTime, ArrayList<StopTime> stopTimes, String id, StopTime header){
+        if(departureTime>=86400){
+            departureTime=departureTime-86400;
+        }
+        for(StopTime t : stopTimes){
+            StopTime st = new StopTime(t);
+            st.setTrip(new Trip());
+            st.getTrip().setId(new AgencyAndId());
+            st.getTrip().getId().setId(id);
+            st.setId(globalIdStopTimes);
+            st.getTrip().getId().setAgencyId(t.getTrip().getId().getAgencyId());
+            st.setArrivalTime(departureTime+(t.getArrivalTime()-header.getArrivalTime()));
+            st.setDepartureTime(departureTime+(t.getDepartureTime()-header.getDepartureTime()));
+            globalIdStopTimes++;
+            newStore.saveEntity(st);
+        }
+
 
     }
 
 
-    public void createNewStopTimes(){
-        LocalTime midnight = LocalTime.MIDNIGHT;
-        LocalDate today = LocalDate.now(ZoneId.of("Europe/Madrid"));
-        LocalDateTime todayMidnight = LocalDateTime.of(today, midnight);
+    public void write(){
+        GtfsWriter writer = new GtfsWriter();
+        writer.setOutputLocation(new File("/Users/dchaves/Downloads/gtfsSpainGoogle/CRTM/metro/updated"));
+        try {
+            writer.run(newStore);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+    public HashMap<String, ArrayList<StopTime>> sumaryStopTimes(){
+        Collection<StopTime> stopTimes = oldStore.getAllStopTimes();
+        HashMap<String,ArrayList<StopTime>> summary = new HashMap<>();
+
+        stopTimes.forEach(stopTime -> {
+            if(!summary.containsKey(stopTime.getTrip().getId().getId())){
+                summary.put(stopTime.getTrip().getId().getId(),new ArrayList<>());
+            }
+            summary.get(stopTime.getTrip().getId().getId()).add(stopTime);
+        });
+        return summary;
     }
 
 
