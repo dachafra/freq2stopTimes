3843e08c90c1237b5d21f91388b938344f3ca640
David Chaves <dachafra@gmail.com>
2018-04-06 15:12:45 +0200

    [ADD] configuration file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# freq2stopTimes
-GTFS. Create new stop_times.txt with frequencies for Linked Connections
+# Frequencies to Stop Times
+Infer new trips and stop times of your favorite transport system from the frequencies.txt file of a GTFS model.
+ 
+Before run it, configure the config.json file as follows:
+
+- **path**: path to your uncompressed GTFS.
+- **output**: path to the new GTFS.
+- **ZoneId**: the [ZoneId](https://www.iana.org/time-zones) of the GTFS
--- src/main/java/dchaves/oeg/dia/fi/upm/es/Main.java
+++ src/main/java/dchaves/oeg/dia/fi/upm/es/Main.java
@@ -1,9 +1,13 @@
 package dchaves.oeg.dia.fi.upm.es;
 
 
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.BasicConfigurator;
+import org.json.JSONObject;
 import org.onebusaway.gtfs.impl.GtfsDaoImpl;
 
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Main
@@ -11,11 +15,16 @@ public class Main
     public static void main( String[] args )
     {
         BasicConfigurator.configure();
-        Gtfs2Collection gtfs2Collection = new Gtfs2Collection(args[0]);
-        GtfsDaoImpl gtfs = gtfs2Collection.read();
-        Freq2StopTimes freq2StopTimes = new Freq2StopTimes(gtfs,args[1], args[2]);
-        if(freq2StopTimes.checkFrequencies()){
-            freq2StopTimes.createNewTrips();
+        try {
+            JSONObject config = new JSONObject(IOUtils.toString(new FileReader("config.json")));
+            Gtfs2Collection gtfs2Collection = new Gtfs2Collection(config.getString("path"));
+            GtfsDaoImpl gtfs = gtfs2Collection.read();
+            Freq2StopTimes freq2StopTimes = new Freq2StopTimes(gtfs,config.getString("output"), config.getString("ZoneId"));
+            if(freq2StopTimes.checkFrequencies()){
+                freq2StopTimes.createNewTrips();
+            }
+        }catch (IOException e){
+            System.out.printf("Error reading the configuration file: "+e.getMessage());
         }
 
 
